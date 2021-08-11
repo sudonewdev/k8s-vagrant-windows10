a95d53a756867e3adc498185d3dacb29f544c3ed
sudonewdev <allotjamil@gmail.com>
2021-08-11 11:33:17 +0530

    hk

--- data/join.sh
+++ data/join.sh
@@ -1,1 +1,1 @@
-kubeadm join 172.16.16.10:6443 --token 8u0y3e.gq0id6qrmckfbez2     --discovery-token-ca-cert-hash sha256:123386e9467527ec9dc21c4cd05319c0ef095a44419ca2e6c9c02c2e0ba03e68 
+kubeadm join 172.16.16.10:6443 --token uhz56a.w05i6bpvvpbtcstb --discovery-token-ca-cert-hash sha256:ac759f9b5dda2598ba2ba455b3007b6cdf0d23882d46433b5b22a62e0a17ee20 

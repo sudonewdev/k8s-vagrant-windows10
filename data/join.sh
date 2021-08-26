9264d95aaae0ae03e30659837543481d3150d2e7
sudonewdev <allotjamil@gmail.com>
2021-08-26 16:21:03 +0530

    some fixes

--- data/join.sh
+++ data/join.sh
@@ -1,1 +1,1 @@
-kubeadm join 172.16.16.10:6443 --token dzk5nz.99ab5y05ugxqhfvr --discovery-token-ca-cert-hash sha256:15a75fd4695f19330e2047266981937c575a6e51248cb8071a9d6b3ecc55e596 
+kubeadm join 172.16.16.10:6443 --token e6fztj.9sckz48z9dr4ggye --discovery-token-ca-cert-hash sha256:4336859e52448c946c0dbda68505b419da8c55b5e96ef380c626da23878faaaf 
